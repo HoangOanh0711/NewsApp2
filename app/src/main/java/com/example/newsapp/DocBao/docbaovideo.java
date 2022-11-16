e5cc2ad80d8997708400f9cbaeea0a04e3388c1a
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-11-16 08:43:13 +0700

    vid

--- app/src/main/java/com/example/newsapp/DocBao/docbaovideo.java
+++ app/src/main/java/com/example/newsapp/DocBao/docbaovideo.java
@@ -1,6 +1,12 @@
 package com.example.newsapp.DocBao;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +14,19 @@ import com.example.newsapp.R;
 
 public class docbaovideo extends AppCompatActivity {
 
+    private VideoView vv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_docbaovideo);
+
+        vv = (VideoView) findViewById(R.id.img_anhbao_docbaovideo);
+
+        MediaController mediacontroller = new MediaController(this);
+        mediacontroller.setAnchorView(vv);
+        vv.setMediaController(mediacontroller);
+        vv.setVideoURI(Uri.parse("http://www.demonuts.com/Demonuts/smallvideo.mp4"));
+        //vv.start();
     }
 }
